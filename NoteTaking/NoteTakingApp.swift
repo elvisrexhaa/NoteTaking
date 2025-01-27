c4ce0af2f910990ebd9307672fee70dc5b039f0a
elvisrexhaa <elvis2000@hotmail.co.uk>
2025-01-27 12:00:07 +0000

    Initial Commit

---
+++ NoteTaking/NoteTakingApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteTakingApp.swift
+//  NoteTaking
+//
+//  Created by Elvis Rexha on 27/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct NoteTakingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NoteTakingTests/NoteTakingTests.swift
@@ -0,0 +1,17 @@
+//
+//  NoteTakingTests.swift
+//  NoteTakingTests
+//
+//  Created by Elvis Rexha on 27/01/2025.
+//
+
+import Testing
+@testable import NoteTaking
+
+struct NoteTakingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
